8cfe5c937bfe1630e75154238e02fa9493720173
leejungho-homa <leejho1107@naver.com>
2023-02-24 15:07:50 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
